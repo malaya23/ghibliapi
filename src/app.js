ff9dd5e92f7de63c8c00af8335254c2f80501e99
Rose Ann Colapo <118804073+malaya23@users.noreply.github.com>
2024-04-18 15:36:06 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,53 +1,54 @@
-const jsonServer = require('json-server')
-const clone = require('clone')
-const data = require('../data.json')
+const jsonServer = require('json-server');
+const clone = require('clone');
+const data = require('../data.json');
 const url = require('url');
 
-const app = jsonServer.create()
-const router = jsonServer.router(clone(data))
+const app = jsonServer.create();
+const router = jsonServer.router(clone(data));
 
 app.use((req, res, next) => {
-  if (req.path === '/') return next()
-  router.db.setState(clone(data))
-  next()
-})
+  if (req.path === '/') return next();
+  router.db.setState(clone(data));
+  next();
+});
 
 app.use(jsonServer.defaults({
   logger: process.env.NODE_ENV !== 'production'
-}))
+}));
 
-app.use(router)
+app.use(router);
 
-filterFields = (obj, fields) => {
+const filterFields = (obj, fields) => {
   Object.keys(obj).forEach((prop) => {
     if (!fields.includes(prop)) {
-      delete obj[prop]
+      delete obj[prop];
     }
-  })
-  return obj
-}
+  });
+  return obj;
+};
 
 router.render = (req, res) => {
-  const defaultLimit = 50
-  const maxLimit = 250
-  var data = res.locals.data
-  var query = url.parse(req.originalUrl, true).query
+  const defaultLimit = 50;
+  const maxLimit = 250;
+  let responseData = res.locals.data;
+  const query = url.parse(req.originalUrl, true).query;
+
   if (query.fields) {
-    var fields = query.fields.split(',')
-    if (data.constructor === Array) {
-      data = data.map((obj) => filterFields(obj, fields))
-    }
-    if (data.constructor === Object) {
-      data = filterFields(data, fields)
+    const fields = query.fields.split(',');
+    if (Array.isArray(responseData)) {
+      responseData = responseData.map((obj) => filterFields(obj, fields));
+    } else if (typeof responseData === 'object') {
+      responseData = filterFields(responseData, fields);
     }
   }
+
   if (query.limit) {
-    if (data.constructor === Array) {
-      data.length = Math.min(Math.min(query.limit, data.length), maxLimit)
+    if (Array.isArray(responseData)) {
+      responseData = responseData.slice(0, Math.min(query.limit, maxLimit));
     }
   }
-  res.jsonp(data)
-}
 
+  res.jsonp(responseData);
+};
 
-module.exports = app
+module.exports = app;
